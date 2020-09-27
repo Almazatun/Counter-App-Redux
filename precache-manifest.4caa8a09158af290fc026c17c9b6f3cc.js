7ee71f6e69ac513116554c8f4f2cadc0d6e1bd42
Almaz <atunstyle@gmail.com>
2020-09-27 14:25:14 +0300

    Updates

---
+++ precache-manifest.4caa8a09158af290fc026c17c9b6f3cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64c4ba344f753493c9fe4f73b8695de",
+    "url": "/scoreboard-react-redux/index.html"
+  },
+  {
+    "revision": "b31cee38cc8ea5bc62b0",
+    "url": "/scoreboard-react-redux/static/css/main.cf69db3d.chunk.css"
+  },
+  {
+    "revision": "0ed1c7297004dbcce65a",
+    "url": "/scoreboard-react-redux/static/js/2.9643f540.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/scoreboard-react-redux/static/js/2.9643f540.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b31cee38cc8ea5bc62b0",
+    "url": "/scoreboard-react-redux/static/js/main.d22451e2.chunk.js"
+  },
+  {
+    "revision": "184b88e5f007ad045fd5",
+    "url": "/scoreboard-react-redux/static/js/runtime-main.1156b582.js"
+  },
+  {
+    "revision": "99e0859d9e4c2bfec5bb5a2061f06f35",
+    "url": "/scoreboard-react-redux/static/media/react_redux.99e0859d.png"
+  }
+]);
